25a79e9df3d12a72793910f293eee02d4937d38e
Anurag Kumar <anurag.hcu@gmail.com>
2023-09-29 11:22:47 +0530

    xproblems

---
+++ backend/lc/lc_easy01_test.go
@@ -0,0 +1,11 @@
+package lc
+
+import
+(
+	"fmt"
+	"testing"
+)
+
+func Test01(t *testing.T){
+	fmt.Println("test")
+}
--- backend/lc/lc_easy_test.go
+++ backend/lc/lc_easy_test.go
@@ -2,35 +2,40 @@ package lc
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestFunc(t *testing.T) {
+	ln := lengthOfLastWord("Hello World") //5
+	ln=lengthOfLastWord("   fly me   to   the moon  ") //4
+	ln=lengthOfLastWord("luffy is still joyboy")//6
+	fmt.Println(ln)
 	// Starter();
 	haystack := "sadbutsad"
 	needle := "sad"
-	res:=findIndexOfFirstOccurence(haystack,needle)
+	res := findIndexOfFirstOccurence(haystack, needle)
 	fmt.Println(res)
 	haystack = "leetcode"
 	needle = "leeto"
-	res=findIndexOfFirstOccurence(haystack,needle)
-	
+	res = findIndexOfFirstOccurence(haystack, needle)
+
 }
-func findIndexOfFirstOccurence(heystack string,needle string)(int){
-	z:=0
+func findIndexOfFirstOccurence(heystack string, needle string) int {
+	z := 0
 	for i := 0; i < len(heystack); i++ {
-		if(needle[i]==heystack[i]){
-			allmatched:=false
+		if needle[i] == heystack[i] {
+			allmatched := false
 			for k := i; i < len(needle); i++ {
-				if(needle[k]==heystack[k]){
-					allmatched=true
-				}else{
-					allmatched=false
+				if needle[k] == heystack[k] {
+					allmatched = true
+				} else {
+					allmatched = false
 					z++
 					break
 				}
 			}
-			if(allmatched){
+			if allmatched {
 				return z
 			}
 		}
@@ -38,5 +43,31 @@ func findIndexOfFirstOccurence(heystack string,needle string)(int){
 	return -1
 }
 
+func removeElement(nums []int, val int) int {
 
+	return 0
+}
+func lengthOfLastWord(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+	lastword := 0
+	s=strings.TrimSpace(s)
+	for _, v := range s {
+		
+		if v != ' ' {
+			lastword++
+		}else{
+			lastword=0
+		}
+		
+	}
+	return lastword
+}
+func plusOne(digits []int) []int {
+	rarray:=make([]int,len(digits))
 
+	
+
+	return rarray
+}
